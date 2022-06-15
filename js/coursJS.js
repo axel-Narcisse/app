4cf9893014ed3aaae0bc2faceee0651f1e9dbc8c
axel <axelnarcissedev@gmail.com>
2022-06-16 00:41:43 +0200

    if/else ajout

--- js/coursJS.js
+++ js/coursJS.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // code
+    *{
+        margin: 0;
+        padding: 0;
+    }
+    
+    header{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        background: chocolate;
+        padding: 9px;
+    }
 });
